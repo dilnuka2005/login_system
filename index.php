7f5ab776a851b54df127f628f4eacbf94b204003
Isara Dilnuka <isaradilnuka2005@gmail.com>
2025-05-08 12:09:49 +0530

    index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<html lang="si">
+<head>
+  <meta charset="UTF-8">
+  <title>Login</title>
+  <link rel="stylesheet" href="assets/style.css">
+</head>
+<body>
+  <h2>පිවිසෙන්න</h2>
+  <form method="POST" action="login.php">
+    <input type="text" name="identifier" placeholder="Username / Email / Phone" required><br>
+    <input type="password" name="password" placeholder="මුරපදය" required><br>
+    <button type="submit">Login</button>
+  </form>
+  <p><a href="register.php">නව ගිණුමක් සාදන්න</a></p>
+  <p><a href="forgot-password.php">මුරපදය අමතකද?</a></p>
+</body>
+</html>
